91418f95213d5ba9e70387ff3bf27070a5844cf1
Jakub Suchy <github@jakubsuchy.com>
2015-11-07 21:48:37 +0100

    add mockup config without passwords

---
+++ template/config/config_mockup.php
@@ -0,0 +1,17 @@
+<?php
+$db_ip = "";
+$db_login="";
+$db_pass ="";
+$db_port = ;
+
+
+$db_web_host = ".wedos.net";
+$db_web_port = "";
+$db_web_login = "";
+$db_web_pass = "";
+$db_web_name = "";
+
+
+
+$link_VIP = "";
+?>
