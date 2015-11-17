50f26eb0a3b7df5e8d4903a2a865fdee46c97eef
Erik Virág <meverik1923@gmail.com>
2015-11-17 12:50:35 +0100

    Create about us

---
+++ template/about/index.php
@@ -0,0 +1,19 @@
+<style>
+    div .content p, .end{
+        margin: 1.2em;
+    }
+    div .content li {
+        margin: 3px;
+    }
+</style>
+
+<h2 class="pages-title">
+    <?php
+    echo $translation["page"]["about"];
+    ?>
+</h2>
+<div class="content pages-content">
+    <?php
+    echo $translation['page']['content'];
+    ?>
+</div>
---
+++ template/config/translation_about.php
@@ -0,0 +1,218 @@
+<?php
+
+function getTranslation($lang)
+{
+    if ($lang == "cz") return array(
+        "page" => array(
+            "about" => "O nás",
+            "content" => "<p>
+        Herní portál <a href=\"http://ezpz.cz/\">EzPz.cz</a> vznikl po rozpadu portálu SOE.cz. Dva tamější hlavní admini,
+        Meverik a Ushirew, se poté rozhodli založit nový herní portál a zároveň do něj převzít bývalé členy realizačního
+        týmu SOE.cz. A tak došlo k založení <a href=\"http://ezpz.cz/\">EzPz.cz</a>, kam se přesunul prakticky celý tým
+        SOE.cz.
+    </p>
+
+    <p>
+        Naší hlavní činností je provoz <a href=\"http://ezpz.cz/page/servers\">kvalitních serverů</a> pro hru
+        Counter-Strike:
+        Global Offensive. Dále na adrese <a href=\"ts3server://ts.ezpz.cz\">ts.ezpz.cz</a> nabízíme veřejný TeamSpeak3
+        server
+        (máte-li zájem o permanentní místnost, <a href=\"http://ezpz.cz/viewtopic.php?f=10&t=20\">napište nám</a>).
+    </p>
+
+    <p>
+        <strong>Mezi přednosti našich CS:GO serverů především patří:</strong>
+
+    <ul>
+        <li><strong>výkonný hardware</strong> – je tak zajištěna naprostá plynulost hry</li>
+        <li><strong>128tick</strong> – komunikace mezi hráči a serverem je u nás (narozdíl od Valve serverů) 2x
+            rychlejší,
+            což zajišťuje přesné hitboxy a celkově lepší požitek ze hry (128tick používají i profesionální servery v
+            ligách
+            FaceIt nebo ESEA)
+        </li>
+        <li><strong>skiny zbraní</strong> – pomocí příkazu !ws si u nás můžete na jakoukoliv zbraň nastavit libovolný
+            skin
+            (dokonce se to vztahuje i na bombu :)
+        </li>
+        <li><strong>nože</strong> – pomocí příkazu !knife si lze nastavit jakýkoliv nůž (a klidně ho dále oskinovat
+            pomocí
+            !ws)
+        </li>
+        <li><strong><a href=\"http://stats.ezpz.cz/\">HLStats</a></strong> – detailní statistiky (nejen) hráčů</li>
+        <li><strong><a href=\"http://ezpz.cz/page/report-system\">Report System</a></strong> – náš vlastnoručně vytvořený
+            systém, skrz který mohou hráči nahlašovat hráče porušující pravidla. Tato hlášení se pak objeví na <a
+                href=\"http://ezpz.cz/page/report-system\">stránce Report Systému</a> a jsou vyřizována našimi adminy.
+        </li>
+    </ul>
+    </p>
+
+    <p>
+        Naší prioritou je i spokojenost hráčů, která je na serverech bohužel často narušována cheatery a neslušnými
+        hráči.
+        Práci herních adminů velmi ulehčují tyto, většinou vlastnoručně vytvořené, nástroje, pomocí kterých lze zpětně
+        dohledat data o hráčích:
+    <ul>
+        <li><strong><a href=\"http://ezpz.cz/page/utilities-connectlog\">Connect Log</a></strong> – výpis všech příchodů a
+            odchodů hráčů na našich serverech
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/utilities-chatlog\">Chat Log</a></strong> – výpis chatu na našich
+            serverech
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/utils-gotv\">GOTV dema</a></strong> – stažení všech dem až 2 týdny do
+            minulosti
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/report-system\">Report System</a></strong></li>
+        <li><strong><a href=\"http://sourcebans.ezpz.cz/\">SourceBans</a></strong> – přehledná správa banů</li>
+    </ul>
+    </p>
+
+    <p>
+        Bohužel náš portál nemá aktivního sponzora, tudíž náklady na provoz
+        jsou placeny námi a in-game MOTD reklamami, které však nejsou
+        otravné, protože se zobrazují jen jednou při připojení na server.
+    </p>
+
+    <p>
+        Jestli bude i nadále trvat zájem hráčů o naše servery, tak v
+        budoucnu určitě otevřeme nové, abychom vyšli vstříc co nejširší
+        skupině hráčů – ať už se jedná o hráče, kteří hru berou vážněji,
+        nebo hráče, kteří si prostě chtějí oddechnout a užít si trochu
+        srandy.
+    </p>
+
+    <p>
+        Děkujeme za Váš zájem a podporu a velmi rádi Vás, hráče a hráčky,
+        uvidíme na našich serverech, případně i našem fóru a
+        <a href=\"https://www.facebook.com/ezpz.cz.sk.en/\">Facebooku</a>. Máte-li rádi naše servery a chtěli byste
+        vstoupit
+        do
+        našich řad, přečtěte si <a href=\"http://ezpz.cz/viewtopic.php?f=16&t=30\">Nábor CS:GO adminů – minimální
+            požadavky</a> a pak si případně
+        podejte žádost. Rádi uvítáme nové přátelské posily :)
+    </p>
+
+    <p>
+        Máte-li zájem o spolupráci s naším portálem (např. sponzoring, reklama), kontaktujte nás prosím na e-mail <img
+            src=\"http://ezpz.cz/images/admin@ezpz.cz.png\" alt=\"admin ezpz.cz\" title=\"admin ezpz.cz\"/>, případně zde na
+        fóru formou soukromé zprávy.
+    </p>
+
+
+    <h2 class=\"end\">
+        <strong>
+            <a href=\"http://ezpz.cz/memberlist.php?mode=team\">Váš realizační tým EzPz.cz</a>
+        </strong>
+    </h2>",
+
+        ),
+    );
+
+    if ($lang == "en") return array(
+        "page" => array(
+            "about" => "About us",
+            "content" => "<p>
+        Herní portál <a href=\"http://ezpz.cz/\">EzPz.cz</a> vznikl po rozpadu portálu SOE.cz. Dva tamější hlavní admini,
+        Meverik a Ushirew, se poté rozhodli založit nový herní portál a zároveň do něj převzít bývalé členy realizačního
+        týmu SOE.cz. A tak došlo k založení <a href=\"http://ezpz.cz/\">EzPz.cz</a>, kam se přesunul prakticky celý tým
+        SOE.cz.
+    </p>
+
+    <p>
+        Naší hlavní činností je provoz <a href=\"http://ezpz.cz/page/servers\">kvalitních serverů</a> pro hru
+        Counter-Strike:
+        Global Offensive. Dále na adrese <a href=\"ts3server://ts.ezpz.cz\">ts.ezpz.cz</a> nabízíme veřejný TeamSpeak3
+        server
+        (máte-li zájem o permanentní místnost, <a href=\"http://ezpz.cz/viewtopic.php?f=10&t=20\">napište nám</a>).
+    </p>
+
+    <p>
+        <strong>Mezi přednosti našich CS:GO serverů především patří:</strong>
+
+    <ul>
+        <li><strong>výkonný hardware</strong> – je tak zajištěna naprostá plynulost hry</li>
+        <li><strong>128tick</strong> – komunikace mezi hráči a serverem je u nás (narozdíl od Valve serverů) 2x
+            rychlejší,
+            což zajišťuje přesné hitboxy a celkově lepší požitek ze hry (128tick používají i profesionální servery v
+            ligách
+            FaceIt nebo ESEA)
+        </li>
+        <li><strong>skiny zbraní</strong> – pomocí příkazu !ws si u nás můžete na jakoukoliv zbraň nastavit libovolný
+            skin
+            (dokonce se to vztahuje i na bombu :)
+        </li>
+        <li><strong>nože</strong> – pomocí příkazu !knife si lze nastavit jakýkoliv nůž (a klidně ho dále oskinovat
+            pomocí
+            !ws)
+        </li>
+        <li><strong><a href=\"http://stats.ezpz.cz/\">HLStats</a></strong> – detailní statistiky (nejen) hráčů</li>
+        <li><strong><a href=\"http://ezpz.cz/page/report-system\">Report System</a></strong> – náš vlastnoručně vytvořený
+            systém, skrz který mohou hráči nahlašovat hráče porušující pravidla. Tato hlášení se pak objeví na <a
+                href=\"http://ezpz.cz/page/report-system\">stránce Report Systému</a> a jsou vyřizována našimi adminy.
+        </li>
+    </ul>
+    </p>
+
+    <p>
+        Naší prioritou je i spokojenost hráčů, která je na serverech bohužel často narušována cheatery a neslušnými
+        hráči.
+        Práci herních adminů velmi ulehčují tyto, většinou vlastnoručně vytvořené, nástroje, pomocí kterých lze zpětně
+        dohledat data o hráčích:
+    <ul>
+        <li><strong><a href=\"http://ezpz.cz/page/utilities-connectlog\">Connect Log</a></strong> – výpis všech příchodů a
+            odchodů hráčů na našich serverech
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/utilities-chatlog\">Chat Log</a></strong> – výpis chatu na našich
+            serverech
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/utils-gotv\">GOTV dema</a></strong> – stažení všech dem až 2 týdny do
+            minulosti
+        </li>
+        <li><strong><a href=\"http://ezpz.cz/page/report-system\">Report System</a></strong></li>
+        <li><strong><a href=\"http://sourcebans.ezpz.cz/\">SourceBans</a></strong> – přehledná správa banů</li>
+    </ul>
+    </p>
+
+    <p>
+        Bohužel náš portál nemá aktivního sponzora, tudíž náklady na provoz
+        jsou placeny námi a in-game MOTD reklamami, které však nejsou
+        otravné, protože se zobrazují jen jednou při připojení na server.
+    </p>
+
+    <p>
+        Jestli bude i nadále trvat zájem hráčů o naše servery, tak v
+        budoucnu určitě otevřeme nové, abychom vyšli vstříc co nejširší
+        skupině hráčů – ať už se jedná o hráče, kteří hru berou vážněji,
+        nebo hráče, kteří si prostě chtějí oddechnout a užít si trochu
+        srandy.
+    </p>
+
+    <p>
+        Děkujeme za Váš zájem a podporu a velmi rádi Vás, hráče a hráčky,
+        uvidíme na našich serverech, případně i našem fóru a
+        <a href=\"https://www.facebook.com/ezpz.cz.sk.en/\">Facebooku</a>. Máte-li rádi naše servery a chtěli byste
+        vstoupit
+        do
+        našich řad, přečtěte si <a href=\"http://ezpz.cz/viewtopic.php?f=16&t=30\">Nábor CS:GO adminů – minimální
+            požadavky</a> a pak si případně
+        podejte žádost. Rádi uvítáme nové přátelské posily :)
+    </p>
+
+    <p>
+        Máte-li zájem o spolupráci s naším portálem (např. sponzoring, reklama), kontaktujte nás prosím na e-mail <img
+            src=\"http://ezpz.cz/images/admin@ezpz.cz.png\" alt=\"admin ezpz.cz\" title=\"admin ezpz.cz\"/>, případně zde na
+        fóru formou soukromé zprávy.
+    </p>
+
+
+    <h2 class=\"end\">
+        <strong>
+            <a href=\"http://ezpz.cz/memberlist.php?mode=team\">Váš realizační tým EzPz.cz</a>
+        </strong>
+    </h2>",
+
+        ),
+    );
+
+    return False;
+}
